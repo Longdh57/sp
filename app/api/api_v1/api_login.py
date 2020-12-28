8f48513a6e3a69d9db5a0f36f6fe28c14e6b06b3
Long Dao <long.dh@teko.vn>
2020-12-28 13:59:07 +0700

    add test case user login

--- app/api/api_v1/api_login.py
+++ app/api/api_v1/api_login.py
@@ -6,6 +6,7 @@ from fastapi.security import OAuth2PasswordRequestForm
 
 from app import models
 from app.core.config import settings
+from app.schemas.base import DataResponse
 from app.schemas.token import Token
 from app.schemas.user import User
 from app.services.user import UserService
@@ -14,23 +15,22 @@ from app.core.security import get_password_hash, create_access_token
 router = APIRouter()
 
 
-@router.post("/login", response_model=Token)
+@router.post("/login", response_model=DataResponse[Token])
 def login_access_token(form_data: OAuth2PasswordRequestForm = Depends()) -> Any:
     user = UserService.get_instance().authenticate(email=form_data.username, password=form_data.password)
     if not user:
-        raise HTTPException(status_code=400, detail="Incorrect email or password")
+        raise HTTPException(status_code=400, detail='Incorrect email or password')
     elif not user.is_active:
-        raise HTTPException(status_code=400, detail="Inactive user")
+        raise HTTPException(status_code=400, detail='Inactive user')
     access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
-    return {
-        "access_token": create_access_token(user.id, expires_delta=access_token_expires),
-        "token_type": "bearer",
-    }
+    return DataResponse().success_response({
+        "access_token": create_access_token(user.id, expires_delta=access_token_expires)
+    })
 
 
-@router.post("/login/test-token", response_model=User)
+@router.post("/login/test-token", response_model=DataResponse[User])
 def test_token(current_user: models.User = Depends(UserService.get_instance().get_current_user)) -> Any:
-    return current_user
+    return DataResponse().success_response({current_user})
 
 
 @router.post("/login/get-password-hash")
